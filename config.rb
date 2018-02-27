714bbd669d08fd7b5f6161df96ec45db85fa5b30
Chris Hough <tech@aboutchrishough.com>
2018-02-26 17:07:45 -0800

    google fonts configured

--- config.rb
+++ config.rb
@@ -6,7 +6,6 @@ activate :directory_indexes
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
-set :fonts_dir, 'assets/fonts'
 
 helpers WebpackAssetHelper
 
@@ -14,25 +13,8 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-
 activate :deploy do |deploy|
   deploy.build_before   = true
   deploy.deploy_method  = :git
   deploy.branch         = 'TEST-BRANCH'
 end
-
-# activate :relative_assets
-#
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-#   activate :minify_html
-#   activate :gzip
-#   activate :asset_hash
-# end
-#
-# activate :deploy do |deploy|
-#   deploy.build_before   = true
-#   deploy.deploy_method  = :git
-#   deploy.branch         = 'gh-pages'
-# end
--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,6 +1,5 @@
 require('script-loader!jquery');
 require('script-loader!bootstrap');
-
 require('./vendor/fontawesome');
 
 console.log('TESTGLOBAL');
