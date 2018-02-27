051effc13b1cdee62d02e3965c59428f3ac6b473
Chris Hough <tech@aboutchrishough.com>
2018-02-26 16:53:19 -0800

    setup fontawesome 5 icons

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -1,1 +1,19 @@
-console.log('TESTLOCAL');
+"use strict";
+// https://stackoverflow.com/questions/47711951/font-awesome-5-bundle-via-npm
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid'
+import brands from '@fortawesome/fontawesome-free-brands';
+
+// This enables using FontAwesome in CSS pseudo elements
+// see: https://fontawesome.com/how-to-use/svg-with-js#pseudo-elements
+fontawesome.config.searchPseudoElements = true;
+
+// Icons should be imported individually to keep bundle size down
+fontawesome.library.add(brands.faFacebookSquare, solid.faPhoneSquare);
+
+// If really necessary, entire styles can be loaded instead of specifying individual icons
+//import solid from '@fortawesome/fontawesome-pro-solid';
+//fontawesome.library.add(solid);
+
+// Execute SVG replacement
+fontawesome.dom.i2svg();
