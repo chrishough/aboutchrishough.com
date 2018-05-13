87a1f2467f645006cbf6138fde5a987a147cbffb
Chris Hough <tech@aboutchrishough.com>
2018-05-13 16:54:37 -0700

    navigation js in place

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -1,5 +1,21 @@
-$(document).ready(function() {
+$(document).ready(() => {
   console.log("TEST-SITE-JS");
   console.log("jQuery Version: " + $().jquery);
   console.log("Bootstrap Version: " + $.fn.tooltip.Constructor.VERSION);
+});
+
+$(document).ready(() => {
+  const $navBarHeader = $(".navbar-header");
+  const $navButton = $("#nav-button");
+  const $navToggler = $(".navbar-toggler");
+
+  $navBarHeader.on('click', () => {
+    if($navToggler.find('svg').attr('data-icon') === "angle-double-up") {
+      console.log('a');
+      $navToggler.find('svg').attr('data-icon', "angle-double-down");
+    } else {
+        console.log('b');
+      $navToggler.find('svg').attr('data-icon', "angle-double-up");
+    }
+  });
 });
