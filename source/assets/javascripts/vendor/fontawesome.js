6611c6217607833ad9b57aec802646ac312ceb9e
Chris Hough <tech@aboutchrishough.com>
2018-05-13 15:56:02 -0700

    more navigation styling, page navigation

--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -3,13 +3,13 @@
 // https://stackoverflow.com/questions/47711951/font-awesome-5-bundle-via-npm
 import fontawesome from '@fortawesome/fontawesome';
 import faAngleDoubleDown from '@fortawesome/fontawesome-free-solid/faAngleDoubleDown';
-
+import faAngleDoubleUp from '@fortawesome/fontawesome-free-solid/faAngleDoubleUp';
 // This enables using FontAwesome in CSS pseudo elements
 // see: https://fontawesome.com/how-to-use/svg-with-js#pseudo-elements
 fontawesome.config.searchPseudoElements = true;
 
 // Icons should be imported individually to keep bundle size down
-fontawesome.library.add(faAngleDoubleDown);
+fontawesome.library.add(faAngleDoubleDown, faAngleDoubleUp);
 
 // If really necessary, entire styles can be loaded instead of specifying individual icons
 // import solid from '@fortawesome/fontawesome-pro-solid';
