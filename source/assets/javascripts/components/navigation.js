06a5ae8d445830a1d6e497c592f7f3f4ff8e1c9c
Chris Hough <chrismhough@gmail.com>
2018-05-24 11:44:15 -0700

    responsive navigation updates

--- source/assets/javascripts/components/navigation.js
+++ source/assets/javascripts/components/navigation.js
@@ -1,5 +1,7 @@
 export const navigation = () => {
   const $navHeaderWrapper = $(".navbar-header-wrapper");
+  const $navItems = $(".navbar-nav > li");
+  const $navDescription = $("#nav-description");
 
   $navHeaderWrapper.on('click', () => {
     if($navHeaderWrapper.find('svg').attr('data-icon') === "angle-double-up") {
@@ -8,4 +10,14 @@ export const navigation = () => {
       $navHeaderWrapper.find('svg').attr('data-icon', "angle-double-up");
     }
   });
+
+  // $navItems.hover(
+  //   function() {
+  //     $navDescription.html($(this).find("span").html());
+  //     $navDescription.stop(true,true).fadeIn("slow");
+  //   }, function() {
+  //     $navDescription.stop(true,true).fadeOut();
+  //     $navDescription.html('&nbsp;');
+  //   }
+  // );
 };
