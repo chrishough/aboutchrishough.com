bc8d2a946890bc07f3c373d6a2758ae158261a25
Chris Hough <chrismhough@gmail.com>
2018-06-08 08:02:22 -0700

    Fixed bottom navigation responsive javascript bugs Fixed header and footer navigation, made the sticky

--- source/assets/javascripts/vendor/breakpoints.js
+++ source/assets/javascripts/vendor/breakpoints.js
@@ -4,6 +4,14 @@ export const breakpoints = () => {
       min: 0,
       max: 576
     },
+    minimium: {
+      min: 350,
+      max: 576
+    },
+    transition: {
+      min: 450,
+      max: 576
+    },
     sm: {
       min: 576,
       max: 768
@@ -14,6 +22,10 @@ export const breakpoints = () => {
     },
     lg: {
       min: 992,
+      max: 1200
+    },
+    maximum: {
+      min: 1000,
       max: 1200
     },
     xl: {
--- source/assets/javascripts/vendor/fontawesome.js
+++ source/assets/javascripts/vendor/fontawesome.js
@@ -6,11 +6,19 @@ import faAngleDoubleDown from '@fortawesome/fontawesome-free-solid/faAngleDouble
 import faAngleDoubleUp from '@fortawesome/fontawesome-free-solid/faAngleDoubleUp';
 import faLightbulb from '@fortawesome/fontawesome-free-solid/faLightbulb';
 import faHeart from '@fortawesome/fontawesome-free-solid/faHeart';
+import faRoad from '@fortawesome/fontawesome-free-solid/faRoad';
 import faEye from '@fortawesome/fontawesome-free-solid/faEye';
 import faMapSigns from '@fortawesome/fontawesome-free-solid/faMapSigns';
 import faMapMarkerAlt from '@fortawesome/fontawesome-free-solid/faMapMarkerAlt';
 import faArrowCircleDown from '@fortawesome/fontawesome-free-solid/faArrowCircleDown';
+import faWrench from '@fortawesome/fontawesome-free-solid/faWrench';
 import faArrowCircleRight from '@fortawesome/fontawesome-free-solid/faArrowCircleRight';
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
+import faTwitter from '@fortawesome/fontawesome-free-brands/faTwitter';
+import faFacebookF from '@fortawesome/fontawesome-free-brands/faFacebookF';
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope';
 // This enables using FontAwesome in CSS pseudo elements
 // see: https://fontawesome.com/how-to-use/svg-with-js#pseudo-elements
 fontawesome.config.searchPseudoElements = true;
@@ -24,6 +32,14 @@ fontawesome.library.add(faAngleDoubleDown,
                         faMapMarkerAlt,
                         faArrowCircleRight,
                         faArrowCircleDown,
+                        faLinkedinIn,
+                        faWrench,
+                        faInstagram,
+                        faTwitter,
+                        faGithub,
+                        faEnvelope,
+                        faFacebookF,
+                        faRoad,
                         faHeart,
                         faEye);
 
