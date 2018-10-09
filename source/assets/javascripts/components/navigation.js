4f9aa7dbbaab1eaf78cc92ef1da173d1911354fc
Chris Hough <chrismhough@gmail.com>
2018-10-08 20:57:15 -0700

    career path icons and stubbed text border alignments and color padding to match contact form embed spacing and heights breadcrumbs blog stubbed with disclaimer re-orged partials

--- source/assets/javascripts/components/navigation.js
+++ source/assets/javascripts/components/navigation.js
@@ -21,7 +21,7 @@ export default () => {
     enter() {
       $navDescriptionDefaultIcon.html('');
       $navbarToggler.attr('data-toggle', 'collapse');
-      $navHeaderWrapper.on('click', () => {
+      $navbarToggler.on('click', () => {
         if ($('.navbar-header-wrapper').find('svg').attr('data-icon') === 'angle-double-up') {
           $('.navbar-header-wrapper').find('svg').attr('data-icon', 'angle-double-down');
         } else {
@@ -31,7 +31,7 @@ export default () => {
     },
     leave() {
       $navbarToggler.attr('data-toggle', '');
-      $navHeaderWrapper.off('click');
+      $navbarToggler.off('click');
     },
   });
 
